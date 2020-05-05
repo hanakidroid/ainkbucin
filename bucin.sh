b9ed15ab31ed3a891fa4b20386f697806e88cbf6
hanakidroid <62693543+hanakidroid@users.noreply.github.com>
2020-05-05 12:42:22 +0700

    Update bucin.sh

--- bucin.sh
+++ bucin.sh
@@ -1,10 +1,37 @@
-Aku seorang introvert boy
-Aku juga seorang coldboy
-Kehidupanku mulai berubah sejak aku masuk SMP
-Aku hampir putus asa karna hidup ku bisa dibilang no love
-Tapi kini ku menemukan seseorang yang aku sukai
-Apa kamu mau menerima ku?
+# QUESTION 1
+NamaGebetan = input('masukan nama kamu :) :\n')
+print('------------------------')
 
-Silahkan masukan jawaban mu :
+# GANTI "X" MENJADI NAMA LU ORANG CONTOH "FABIAN" MENGGUNAKAN TANDA PETIK
 
-[Mau/Tidak] 
+NamaLuPada = "X"
+
+# QUESTION 2 
+Question = input('Mau ga jadian sama %s ? \nA.iya \nB.Tidak \nMasukan jawaban:' % NamaLuPada) 
+
+# ANSWER
+
+if Question == "A" or Question == "iya" or Question == "a" or Question == "iya" or Question == "IYA" :
+    print('\n')
+    x = "love"
+    print("\t  love\t      love")
+    print("\t"+str(x*2)+"    "+str(x*2))
+    print("      "+str(x*6))
+    print(str(" "*5)+str(x*6)+"lo")
+    print(str(" "*5)+str(x*6)+"lo")
+    print("      "+str(x*6))
+    print("\t"+str(x*5))
+    print("\t"+str(" "*2)+str(x*4)+"l")
+    print("\t"+str(" "*4)+str(x*3)+"l")
+    print("\t"+str(" "*7)+str(x*2))
+    print("\t"+str(" "*10)+"v")
+    print('------------------------')
+    print("\nI LOVE YOUUUU" , NamaGebetan)
+    print("Terimakasih atas pilihan nya :)")
+    print("- %s" % NamaLuPada) 
+    print('------------------------')
+else:
+	print('Thanks %s udah mau ngejawab hehe ^_^' % NamaGebetan)
+	print('Good bye %s :)' % NamaGebetan)
+
+# END
